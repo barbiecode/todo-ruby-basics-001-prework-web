f9b9e45d2708a0c6a5a7095d8f3e06c987623dbf
barbiecode <helesa@gmail.com>
2016-03-10 09:26:52 -0500

    Commit changes to lib/ruby_basics.rb

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
-def division(num1, num2)
- 
+def division(num1,num2)
+    division= num1/num2
 end
 
 def assign_variable(value)
-  
+  assign_variable = value
 end
 
-def argue
-  
+def argue(phrase="I'm right and you are wrong!")
+  argue = "#{phrase}"
 end
 
-def greeting
-
+def greeting(greeting, name)
+    "#{greeting}, #{name}!"
 end
 
 def return_a_value
+    return_a_value = "Nice"
 
 end
 
 def last_evaluated_value
+    last_evaluated_value = "expert"
 
 end
 
-def pizza_party
-
+def pizza_party(toppings = "cheese")
+    pizza_party = toppings
 end
